c88b8785e4eacacb9ed63753567e2e3f580eecaf
StartingCoding <StartingCodingPartner@gmail.com>
2019-04-02 11:46:14 +0200

    Start Project 4.

---
+++ Project 4/Project 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 4
+//
+//  Created by Loris on 02/04/2019.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
