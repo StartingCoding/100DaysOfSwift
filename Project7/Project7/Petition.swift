2c4791385208579a329798032b44fd0031478ddf
StartingCoding <StartingCodingPartner@gmail.com>
2019-04-12 14:33:21 +0200

    Finished Project 7 Part 1.

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Loris on 4/12/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Loris on 4/12/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = URL(string: "https://api.whitehouse.gov/v1/petitions.json?limit=100") {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let data = try? decoder.decode(Petitions.self, from: json) {
+            petitions = data.results
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+}
+
