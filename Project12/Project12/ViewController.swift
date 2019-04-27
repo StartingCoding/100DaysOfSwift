abc33ce8f12b5242d6dffc619de9df9facef4fbb
StartingCoding <StartingCodingPartner@gmail.com>
2019-04-27 22:55:40 +0200

    Started Project 12 Part 1.

---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Loris on 4/27/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBool = defaults.bool(forKey: "UseFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "dict") as? [String: String] ?? [String: String]()
+    }
+
+
+}
+
