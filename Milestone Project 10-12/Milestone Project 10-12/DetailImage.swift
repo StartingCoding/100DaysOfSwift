abc89f27b9644c5f0c931a2a835199e93fdd181b
StartingCoding <StartingCodingPartner@gmail.com>
2019-04-29 22:14:02 +0200

    Started Milestone Project 10-12.

---
+++ Milestone Project 10-12/Milestone Project 10-12/DetailImage.swift
@@ -0,0 +1,20 @@
+//
+//  DetailImage.swift
+//  Milestone Project 10-12
+//
+//  Created by Loris on 4/29/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class DetailImage: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Milestone Project 10-12/Milestone Project 10-12/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Milestone Project 10-12
+//
+//  Created by Loris on 4/29/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var caption = [String]()
+    var images = [Data]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Photos"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(takePic))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return caption.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Caption", for: indexPath)
+        cell.textLabel?.text = caption[indexPath.row]
+        return cell
+    }
+    
+    @objc func takePic() {
+        let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as? UIImage
+        let imageData = image?.jpegData(compressionQuality: 0.8)
+        if let imageData = imageData {
+            images.insert(imageData, at: 0)
+            caption.insert("test", at: 0)
+            tableView.reloadData()
+        }
+        picker.dismiss(animated: true)
+    }
+}
+
