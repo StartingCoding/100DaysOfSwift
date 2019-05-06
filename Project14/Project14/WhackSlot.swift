8c760ac4325da92b636926b30d996fd9a9494497
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-06 22:02:28 +0200

    Finished Project 14 Part 1.

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -12,6 +12,9 @@ import UIKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -21,7 +24,7 @@ class WhackSlot: SKNode {
         let cropNode = SKCropNode()
         cropNode.position = CGPoint(x: 0, y: 15)
         cropNode.zPosition = 1
-        cropNode.maskNode = nil
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
         
         charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
@@ -29,5 +32,34 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+        
+        
     }
 }
