80bd5af0cb86843338b946565620606561d87fa6
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-10 23:27:12 +0200

    Finished Milestone Project 10-12.

---
+++ Milestone Project 10-12/Milestone Project 10-12/Picture.swift
@@ -0,0 +1,14 @@
+//
+//  Picture.swift
+//  Milestone Project 10-12
+//
+//  Created by Loris on 5/10/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import Foundation
+
+struct Picture: Codable {
+    var imageName: String
+    var caption: String
+}
--- Milestone Project 10-12/Milestone Project 10-12/ViewController.swift
+++ Milestone Project 10-12/Milestone Project 10-12/ViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    var caption = [String]()
-    var images = [UIImage]()
+    var pictures = [Picture]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +17,33 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
         title = "Photos"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(takePic))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPic = defaults.object(forKey: "pictures") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                pictures = try jsonDecoder.decode([Picture].self, from: savedPic)
+            } catch {
+                print("Failed to save Pics.")
+            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return caption.count
+        return pictures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Caption", for: indexPath)
-        cell.textLabel?.text = caption[indexPath.row]
+        cell.textLabel?.text = pictures[indexPath.row].caption
+        cell.imageView?.image = UIImage(contentsOfFile: pictures[indexPath.row].imageName)
         return cell
     }
     
@@ -48,17 +65,45 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         
-        images.insert(image, at: 0)
-        caption.insert("Photo", at: 0)
-        tableView.reloadData()
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
         
-        dismiss(animated: true)
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true) {
+            let ac = UIAlertController(title: "Caption", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] _ in
+                guard let caption = ac?.textFields?[0].text else { return }
+                
+                let picture = Picture(imageName: imagePath.path, caption: caption)
+                self?.pictures.append(picture)
+                self?.savePic()
+                self?.tableView.reloadData()
+            }))
+            self.present(ac, animated: true)
+        }
+    }
+    
+    func savePic() {
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedPic = try? jsonEncoder.encode(pictures) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedPic, forKey: "pictures")
+        } else {
+            print("Failed to save Pics.")
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailImage") as? DetailImage {
-            vc.image = images[indexPath.row]
-            vc.photoName = caption[indexPath.row]
+            
+            vc.image = UIImage(contentsOfFile: pictures[indexPath.row].imageName)
+            vc.photoName = pictures[indexPath.row].caption
             navigationController?.pushViewController(vc, animated: true)
         }
     }
