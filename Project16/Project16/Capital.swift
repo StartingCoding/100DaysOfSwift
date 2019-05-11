8f2966a065d6941ae52d59efdd2cc5642e17ad56
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-11 12:17:00 +0200

    Finished Project 16 Part 1.

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Loris on 5/11/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
