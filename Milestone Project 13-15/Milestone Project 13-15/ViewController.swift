75637e0f3f2381f8c379de73dba4d55d9e393714
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-17 21:25:42 +0200

    Started Milestone Project 13-15.

---
+++ Milestone Project 13-15/Milestone Project 13-15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone Project 13-15
+//
+//  Created by Loris on 5/17/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// https://restcountries.eu
+}
+
