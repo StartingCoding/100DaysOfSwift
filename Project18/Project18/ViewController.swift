df05b93314cd48587a8ea3d4235ce149344b7948
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-22 09:45:28 +0200

    Finished Project 18 Part 1.

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Loris on 5/21/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
