6d2b051d1882689068c77f51f50f4968b5ac7ac6
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-22 15:01:45 +0200

    Continuing Milestone Project 13-15.

---
+++ Milestone Project 13-15/Milestone Project 13-15/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  Milestone Project 13-15
+//
+//  Created by Loris on 5/22/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Codable {
+    var name: String
+    var capital: String
+    var population: Int
+}
--- Milestone Project 13-15/Milestone Project 13-15/ViewController.swift
+++ Milestone Project 13-15/Milestone Project 13-15/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countries = [Country]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Country Facts"
+        
+        if let urlPath = Bundle.main.url(forResource: "aruba", withExtension: "json") {
+            if let data = try? Data(contentsOf: urlPath) {
+                parse(json: data)
+            }
+        }
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].name
+        return cell
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let data = try? decoder.decode([Country].self, from: json) {
+            countries = data
+        }
     }
 
 // https://restcountries.eu
