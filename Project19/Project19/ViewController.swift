d724d7a4a7de0f79249995c293b82dbabd8abd3a
StartingCoding <StartingCodingPartner@gmail.com>
2019-05-24 15:05:17 +0200

    Finished Project 19 Part 1.

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Loris on 5/24/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
