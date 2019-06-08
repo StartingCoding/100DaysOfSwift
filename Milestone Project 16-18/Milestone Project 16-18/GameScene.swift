e04f48a0327e5baa4b8fff6dad0fe20ac76c8a67
StartingCoding <StartingCodingPartner@gmail.com>
2019-06-08 11:32:37 +0200

    Continuing Milestone Project 16-18 "Shooting Gallery Game". - created Starting Animation for the background - created the timer of the game

--- Milestone Project 16-18/Milestone Project 16-18/GameScene.swift
+++ Milestone Project 16-18/Milestone Project 16-18/GameScene.swift
@@ -9,7 +9,22 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var waterBg: SKSpriteNode!
+    var waterFg: SKSpriteNode!
+    
     var target0: SKSpriteNode!
+    
+    var timeLabel: SKLabelNode!
+    var minute = 60 {
+        willSet {
+            timeLabel.text = "\(newValue)"
+        }
+    }
+    
+    var gameOverLabel: SKSpriteNode!
+    var isGameOver: Bool = false
+    
+    var gameTimer: Timer?
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "wood-background")
@@ -31,22 +46,84 @@ class GameScene: SKScene {
         grassTrees.zPosition = 1
         addChild(grassTrees)
         
-        let waterBg = SKSpriteNode(imageNamed: "water-bg")
+        waterBg = SKSpriteNode(imageNamed: "water-bg")
         waterBg.position = CGPoint(x: 512, y: 280)
         waterBg.size.width = view.frame.size.width
         waterBg.zPosition = 2
         addChild(waterBg)
         
-        let waterFg = SKSpriteNode(imageNamed: "water-fg")
+        waterFg = SKSpriteNode(imageNamed: "water-fg")
         waterFg.position = CGPoint(x: 512, y: 170)
         waterFg.size.width = view.frame.size.width
-        waterFg.zPosition = 3
+        waterFg.zPosition = 4
         addChild(waterFg)
         
+        timeLabel = SKLabelNode(fontNamed: "Chalkduster")
+        timeLabel.position = CGPoint(x: 512, y: 60)
+        timeLabel.zPosition = 6
+        timeLabel.text = "60"
+        timeLabel.fontSize = 44
+        addChild(timeLabel)
+        
+        startGame()
+    }
+    
+    @objc func startGame() {
+        // Start to move the water on the background
+        createStartingAnimation()
+        
+        // Fire a timer every one second so it updates the label to display how many seconds remain and creates enemy
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(manageTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func manageTime() {
+        minute -= 1
+        
+        // Create enemy
+        createEnemy()
+        
+        // If minute is zero, the time is over so invalidate the timer
+        if minute == 0 {
+            gameTimer?.invalidate()
+            
+            isGameOver = true
+            
+            // Display a GameOver image
+            gameOverLabel = SKSpriteNode(imageNamed: "game-over")
+            gameOverLabel.zPosition = 6
+            gameOverLabel.position = CGPoint(x: 512, y: 384)
+            gameOverLabel.alpha = 0
+            gameOverLabel.run(SKAction.fadeIn(withDuration: 1))
+            addChild(gameOverLabel)
+        }
+    }
+    
+    func createEnemy() {
+        guard !isGameOver else { return }
+        
         target0 = SKSpriteNode(imageNamed: "target0")
-        target0.position = CGPoint(x: 900, y: 290)
-        target0.zPosition = 4
+        target0.position = CGPoint(x: 960, y: 290)
+        target0.zPosition = 3
         addChild(target0)
+        
+        let moveTargetOnLine = SKAction.moveTo(x: 0, duration: 1)
+        target0.run(moveTargetOnLine)
+    }
+    
+    func createStartingAnimation() {
+        // Create steps of the animation
+        let moveLineForward = SKAction.moveTo(x: 562, duration: 0.8)
+        let moveLineBackward = SKAction.moveTo(x: 472, duration: 0.8)
+        
+        // Create forever animation for the first line
+        let moveLineForeground = SKAction.sequence([moveLineForward, moveLineBackward])
+        let moveLineForegroundForever = SKAction.repeatForever(moveLineForeground)
+        waterFg.run(moveLineForegroundForever)
+        
+        // Create forever animation for the second line backward
+        let moveLineBackground = SKAction.sequence([moveLineBackward, moveLineForward])
+        let moveLineBackgroundForever = SKAction.repeatForever(moveLineBackground)
+        waterBg.run(moveLineBackgroundForever)
     }
     
     override func update(_ currentTime: TimeInterval) {
