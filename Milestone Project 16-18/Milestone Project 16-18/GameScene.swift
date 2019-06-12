f0c672e63a76b257882223d77cb18353310d4df2
StartingCoding <StartingCodingPartner@gmail.com>
2019-06-12 11:13:31 +0200

    Continued Milestone Project 16-18 "Shooting Gallery Game". - Started to add interaction with TouchesBegan() - Randomized spawn of targets - Added different animations for different targets - Add logic to remove nodes out of view

--- Milestone Project 16-18/Milestone Project 16-18/GameScene.swift
+++ Milestone Project 16-18/Milestone Project 16-18/GameScene.swift
@@ -12,7 +12,7 @@ class GameScene: SKScene {
     var waterBg: SKSpriteNode!
     var waterFg: SKSpriteNode!
     
-    var target0: SKSpriteNode!
+    var target: SKSpriteNode!
     
     var timeLabel: SKLabelNode!
     var minute = 60 {
@@ -26,6 +26,13 @@ class GameScene: SKScene {
     
     var gameTimer: Timer?
     
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "wood-background")
         background.blendMode = .replace
@@ -37,33 +44,41 @@ class GameScene: SKScene {
         let curtainBackground = SKSpriteNode(imageNamed: "curtains")
         curtainBackground.position = CGPoint(x: 512, y: 384)
         curtainBackground.size = view.frame.size
-        curtainBackground.zPosition = 5
+        curtainBackground.zPosition = 100
         addChild(curtainBackground)
         
         let grassTrees = SKSpriteNode(imageNamed: "grass-trees")
         grassTrees.position = CGPoint(x: 512, y: 440)
         grassTrees.size.width = view.frame.size.width
-        grassTrees.zPosition = 1
+        grassTrees.zPosition = 2
         addChild(grassTrees)
         
         waterBg = SKSpriteNode(imageNamed: "water-bg")
         waterBg.position = CGPoint(x: 512, y: 280)
         waterBg.size.width = view.frame.size.width
-        waterBg.zPosition = 2
+        waterBg.zPosition = 4
         addChild(waterBg)
         
         waterFg = SKSpriteNode(imageNamed: "water-fg")
         waterFg.position = CGPoint(x: 512, y: 170)
         waterFg.size.width = view.frame.size.width
-        waterFg.zPosition = 4
+        waterFg.zPosition = 6
         addChild(waterFg)
         
         timeLabel = SKLabelNode(fontNamed: "Chalkduster")
         timeLabel.position = CGPoint(x: 512, y: 60)
-        timeLabel.zPosition = 6
+        timeLabel.zPosition = 101
         timeLabel.text = "60"
         timeLabel.fontSize = 44
         addChild(timeLabel)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 120, y: 60)
+        scoreLabel.zPosition = 101
+        scoreLabel.fontSize = 44
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
         
         startGame()
     }
@@ -80,7 +95,7 @@ class GameScene: SKScene {
         minute -= 1
         
         // Create enemy
-        createEnemy()
+        spawnEnemy()
         
         // If minute is zero, the time is over so invalidate the timer
         if minute == 0 {
@@ -90,7 +105,7 @@ class GameScene: SKScene {
             
             // Display a GameOver image
             gameOverLabel = SKSpriteNode(imageNamed: "game-over")
-            gameOverLabel.zPosition = 6
+            gameOverLabel.zPosition = 101
             gameOverLabel.position = CGPoint(x: 512, y: 384)
             gameOverLabel.alpha = 0
             gameOverLabel.run(SKAction.fadeIn(withDuration: 1))
@@ -98,16 +113,65 @@ class GameScene: SKScene {
         }
     }
     
-    func createEnemy() {
+    func spawnEnemy() {
         guard !isGameOver else { return }
         
-        target0 = SKSpriteNode(imageNamed: "target0")
-        target0.position = CGPoint(x: 960, y: 290)
-        target0.zPosition = 3
-        addChild(target0)
+        if Int.random(in: 1...2) == 1 {
+            let numLine = Int.random(in: 1...3)
+            
+            if numLine == 1 {
+                createEnemy(y: 290, zPos: 5)
+            }
+            
+            if numLine == 2 {
+                createEnemy(y: 400, zPos: 3)
+            }
+            
+            if numLine == 3 {
+                createEnemy(y: 510, zPos: 1)
+            }
+        }
+    }
+    
+    func createEnemy(y: Int, zPos: Int) {
+        let randomTarget = Int.random(in: 0...3)
         
-        let moveTargetOnLine = SKAction.moveTo(x: 0, duration: 1)
-        target0.run(moveTargetOnLine)
+        if randomTarget == 0 {
+            target = SKSpriteNode(imageNamed: "target0")
+        } else if randomTarget == 1 {
+            target = SKSpriteNode(imageNamed: "target1")
+        } else if randomTarget == 2 {
+            target = SKSpriteNode(imageNamed: "target2")
+        } else if randomTarget == 3 {
+            target = SKSpriteNode(imageNamed: "target3")
+        } else {
+            target = SKSpriteNode(imageNamed: "target0")
+        }
+        
+        // Set good bad value
+        if randomTarget == 0 {
+            target.name = "badTarget"
+        } else {
+            target.name = "goodTarget"
+        }
+        
+        let moveTargetSlowOnLine = SKAction.moveTo(x: -200, duration: 3)
+        let moveTargetFastOnLine = SKAction.moveTo(x: -200, duration: 1)
+        
+        // Set speed and size of fast target
+        if randomTarget == 3 {
+            target.xScale = 0.7
+            target.yScale = 0.7
+            target.run(moveTargetFastOnLine)
+            target.position = CGPoint(x: 960, y: CGFloat(y - 20))
+        } else {
+            target.run(moveTargetSlowOnLine)
+            target.position = CGPoint(x: 960, y: y)
+        }
+        
+        target.zPosition = CGFloat(zPos)
+        
+        addChild(target)
     }
     
     func createStartingAnimation() {
@@ -126,7 +190,33 @@ class GameScene: SKScene {
         waterBg.run(moveLineBackgroundForever)
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        // Shot sound
+        run(SKAction.playSoundFileNamed("shot.wav", waitForCompletion: false))
+        
+        for node in tappedNodes {
+            if node.name == "badTarget" {
+                node.removeAllActions()
+                node.run(SKAction.fadeOut(withDuration: 0.3))
+            } else if node.name == "goodTarget" {
+                node.removeAllActions()
+                node.run(SKAction.fadeOut(withDuration: 0.3))
+            }
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        // Remove every enemy that is out of the game
+        for node in children {
+            if node.position.x < -100 {
+                node.removeFromParent()
+            }
+        }
     }
 }
