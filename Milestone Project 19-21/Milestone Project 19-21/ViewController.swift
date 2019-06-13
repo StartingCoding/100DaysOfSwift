aac8c7536aec38eb50e793e95d9fc7c58351d779
StartingCoding <StartingCodingPartner@gmail.com>
2019-06-13 14:37:17 +0200

    Start Milestone Project 19-21 "Apple Notes".

---
+++ Milestone Project 19-21/Milestone Project 19-21/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone Project 19-21
+//
+//  Created by Loris on 6/13/19.
+//  Copyright © 2019 Loris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
