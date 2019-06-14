4645217f99473b5c0c6ae4482e1b600aaf844175
StartingCoding <StartingCodingPartner@gmail.com>
2019-06-14 14:26:05 +0200

    Continuing Milestone Project 19-21 "Apple Notes". - Added fix for keyboard bug

--- Milestone Project 19-21/Milestone Project 19-21/DetailViewController.swift
+++ Milestone Project 19-21/Milestone Project 19-21/DetailViewController.swift
@@ -20,6 +20,39 @@ class DetailViewController: UIViewController {
         if let textNote = note {
             textView.text = textNote
         }
+        
+        // Set up notifications when keyboard changing frame every state of its size (show, hide, orientation, QuickType...)
+        // and when hide keyboard animation is finished
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        // UIResponder.keyboardFrameEndUserInfoKey is the frame of the keyboard when its animation has finished
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        // We take the size of the last frame of the kayboard (CGRect containing CGPoint and CGSize)
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        
+        // We need to convert the CGRect of the keyboard to our view's coordinate (Fix if the user rotate the device)
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, to: view.window)
+        
+        // If the keyboard has finished hiding the content inset of textView will be 0
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+        } else {
+            // If the keyboard is not hiding (it's visible) the bottom of the content inset will be the height of the keyboard
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        // Fix the scrolling indicator to not go below the keyboard
+        textView.scrollIndicatorInsets = textView.contentInset
+        
+        // selectedRange is where is the cursor
+        let selectedRange = textView.selectedRange
+        // Scroll the view to the cursor
+        textView.scrollRangeToVisible(selectedRange)
     }
 
 }
