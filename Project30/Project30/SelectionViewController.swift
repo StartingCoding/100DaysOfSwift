380720cdbced340e73b88f255420b8ba3b44d052
StartingCoding <StartingCodingPartner@gmail.com>
2019-08-12 18:56:48 +0200

    Finished Project 30 Part 1 "Instruments".

---
+++ Project30/Project30/SelectionViewController.swift
@@ -0,0 +1,110 @@
+//
+//  SelectionViewController.swift
+//  Project30
+//
+//  Created by TwoStraws on 20/08/2016.
+//  Copyright (c) 2016 TwoStraws. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: UITableViewController {
+	var items = [String]() // this is the array that will store the filenames to load
+	var viewControllers = [UIViewController]() // create a cache of the detail view controllers for faster loading
+	var dirty = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		title = "Reactionist"
+
+		tableView.rowHeight = 90
+		tableView.separatorStyle = .none
+        // Register a new class for tableViewCell so we can dequeue it later on.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+		// load all the JPEGs into our array
+		let fm = FileManager.default
+
+		if let tempItems = try? fm.contentsOfDirectory(atPath: Bundle.main.resourcePath!) {
+			for item in tempItems {
+				if item.range(of: "Large") != nil {
+					items.append(item)
+				}
+			}
+		}
+    }
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		if dirty {
+			// we've been marked as needing a counter reload, so reload the whole table
+			tableView.reloadData()
+		}
+	}
+
+    // MARK: - Table view data source
+
+	override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return items.count * 10
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+		// find the image for this cell, and load its thumbnail
+		let currentImage = items[indexPath.row % items.count]
+		let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
+		let path = Bundle.main.path(forResource: imageRootName, ofType: nil)!
+		let original = UIImage(contentsOfFile: path)!
+
+        // Make a CGRect of the correct size of the table row height
+        let rendererRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
+		let renderer = UIGraphicsImageRenderer(size: rendererRect.size)
+
+		let rounded = renderer.image { ctx in
+            // Make an ellipse in order to clip the original image so it is a circle
+            ctx.cgContext.addEllipse(in: rendererRect)
+            ctx.cgContext.clip()
+
+			original.draw(in: rendererRect)
+		}
+
+		cell.imageView?.image = rounded
+
+        // give the images a nice shadow to make them look a bit more dramatic
+        cell.imageView?.layer.shadowColor = UIColor.black.cgColor
+        cell.imageView?.layer.shadowOpacity = 1
+        cell.imageView?.layer.shadowRadius = 10
+        cell.imageView?.layer.shadowOffset = CGSize.zero
+        // Tell iOS that the shadow is a based on a fixed circle so it doesn't need to calculate every single pixel everytime.
+        cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: rendererRect).cgPath
+
+		// each image stores how often it's been tapped
+		let defaults = UserDefaults.standard
+		cell.textLabel?.text = "\(defaults.integer(forKey: currentImage))"
+
+		return cell
+    }
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let vc = ImageViewController()
+		vc.image = items[indexPath.row % items.count]
+		vc.owner = self
+
+		// mark us as not needing a counter reload when we return
+		dirty = false
+
+		// add to our view controller cache and show
+		viewControllers.append(vc)
+		navigationController!.pushViewController(vc, animated: true)
+	}
+}
